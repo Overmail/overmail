f2e1198463ef298be7a721655f1f72c06554e5f0
Julius Vincent Babies <julius@familie-babies.de>
2026-09-16 22:33:57 +0200

    feat(api,web/devices-settings #58): Generate and display login QR

--- server/src/main/kotlin/es/jvbabi/overmail/server/http/Routes.kt
+++ server/src/main/kotlin/es/jvbabi/overmail/server/http/Routes.kt
@@ -57,6 +57,7 @@ import es.jvbabi.overmail.server.http.webapp.ai.chat.message
 import es.jvbabi.overmail.server.http.webapp.ai.chat.retryMessage
 import es.jvbabi.overmail.server.http.webapp.ai.currentAiConfig
 import es.jvbabi.overmail.server.http.webapp.content.contentSocket
+import es.jvbabi.overmail.server.http.webapp.devices.createAuthCode
 import es.jvbabi.overmail.server.http.webapp.home.homeSocket
 import es.jvbabi.overmail.server.http.webapp.views.viewsSocket
 import io.ktor.http.ContentType
@@ -314,6 +315,14 @@ internal fun Application.configureRouting() {
                 route("/content") {
                     route("/socket") {
                         contentSocket()
+                    }
+                }
+
+                route("/devices") {
+                    route("/auth") {
+                        route("/generate-auth-code") {
+                            createAuthCode()
+                        }
                     }
                 }
 
